9a64e6dea8d5cbf01fa693058ec666bd5e0b37a6
Roman Semirook <semirook@ya.ru>
2011-11-30 16:17:03 +0400

    Ignore .idea folder

--- fabdeploy_extensions/extensions/sphinx.py
+++ fabdeploy_extensions/extensions/sphinx.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
-from fabric.api import run, sudo, settings, env, cd
+from fabric.api import run, sudo, env
 from fab_deploy import utils
 from fab_deploy.system import aptitude_install
-from fab_deploy.crontab import crontab_update
+
 
 __all__ = ['sphinx_install', 'sphinx_setup', 'sphinx_indexer', 'sphinx_start']
 
+def get_sphinx_config():
+    id = env.conf['NAME']
+    settings_file = '_settings/applications/sphinx/sphinx_{0}.conf'.format(id)
 
-SPHINX_CONFIG = '/etc/sphinxsearch/sphinx.conf'
+    return settings_file
+
 
 @utils.run_as_sudo
 def sphinx_install():
-    ''' Install sphinx search '''
+    """ Install sphinx search """
     aptitude_install('sphinxsearch')
     sudo('sed -i "s/START=no/START=yes/g" /etc/default/sphinxsearch')
 
 
 @utils.inside_project
-def sphinx_setup(apps):
-    ''' Sphinx setup '''
-    sudo('DJANGO_SETTINGS_ENVIRONMENT={0} python manage.py generate_sphinx_config {1} > {2}'.format(env.conf['NAME'], apps, SPHINX_CONFIG))
+def sphinx_setup(apps='--all'):
+    """ Sphinx setup """
+    sudo('DJANGO_SETTINGS_ENVIRONMENT={0} python manage.py generate_sphinx_config {1} > {2}'.format(env.conf['NAME'], apps, get_sphinx_config()))
 
     make_log_files()
     sphinx_indexer()
@@ -28,23 +32,33 @@ def sphinx_setup(apps):
 
 @utils.run_as_sudo
 def make_log_files():
-    ''' Generate sphinx config from django '''
-    sphinx_config = run('cat {0}'.format(SPHINX_CONFIG))
+    """ Generate sphinx config from django """
+    sphinx_config = run('cat {0}'.format(get_sphinx_config()))
     for line in sphinx_config.splitlines():
         option = line.replace(' ', '').split('=')
         if len(option) == 2 and option[1].endswith('.log'):
             sudo('touch {0}'.format(option[1]))
 
 
+@utils.inside_project
 @utils.run_as_sudo
 def sphinx_indexer():
-    ''' Setup sphinx indexing '''
-    indexer_command = 'indexer --all --rotate >/dev/null 2>&1'
+    """ Setup sphinx indexing """
+    indexer_command = 'indexer --config {0} --all --rotate'.format(get_sphinx_config())
     sudo(indexer_command)
-    #crontab_update('0-59 * * * * {0}'.format(indexer_command), 'indexer')
+    # Has to reindex with Celery
+    # crontab_update('0-59 * * * * {0}'.format(indexer_command), 'indexer')
 
 
+@utils.inside_project
 @utils.run_as_sudo
 def sphinx_start():
-    ''' Run sphinx daemon '''
-    sudo('/etc/init.d/sphinxsearch start')
+    """ Run searchd daemon """
+    sudo('/usr/bin/searchd --config {0}'.format(get_sphinx_config()))
+
+
+@utils.inside_project
+@utils.run_as_sudo
+def sphinx_stop():
+    """ Stop searchd daemon """
+    sudo('/usr/bin/searchd --config {0} --stop'.format(get_sphinx_config()))
