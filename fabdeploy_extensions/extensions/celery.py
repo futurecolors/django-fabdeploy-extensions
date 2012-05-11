c7fc59023f84b89a5dc6388150f6f406983e36f5
Vitaly Olevinsky <olevinskyvs@gmail.com>
2012-05-11 11:39:38 +0400

    Updated celery config for multi instances.

--- fabdeploy_extensions/extensions/celery.py
+++ fabdeploy_extensions/extensions/celery.py
@@ -14,16 +14,17 @@ __all__ = [
 @utils.run_as_sudo
 def celery_setup():
     """ Setups Celery. """
+
     sudo('adduser --system --no-create-home --disabled-login --disabled-password --group celery')
 
-    utils.upload_config_template('celeryd.sh', '/etc/init.d/celeryd', use_sudo=True)
-    utils.upload_config_template('celeryd.conf', '/etc/default/celeryd', use_sudo=True)
+    utils.upload_config_template('celeryd.sh', '/etc/init.d/celeryd_%s' % env.conf['INSTANCE_NAME'], use_sudo=True)
+    utils.upload_config_template('celeryd.conf', '/etc/default/celeryd_%s' % env.conf['INSTANCE_NAME'], use_sudo=True)
 
     sudo('mkdir -p /var/log/celery')
     sudo('chmod 777 /var/log/celery')
 
-    sudo('chmod +x /etc/init.d/celeryd')
-    sudo('update-rc.d celeryd defaults')
+    sudo('chmod +x /etc/init.d/celeryd_%s' % env.conf['INSTANCE_NAME'])
+    sudo('update-rc.d celeryd_%s defaults' % env.conf['INSTANCE_NAME'])
     celery_start()
 
 
@@ -31,38 +32,38 @@ def celery_setup():
 def celery_start():
     ''' Start Celery '''
     with settings(warn_only=True):
-        sudo('/etc/init.d/celeryd start')
+        sudo('/etc/init.d/celeryd_%s start' % env.conf['INSTANCE_NAME'])
 
 @utils.run_as_sudo
 def celery_stop():
     ''' Stop Celery '''
     with settings(warn_only=True):
-        sudo('/etc/init.d/celeryd stop')
+        sudo('/etc/init.d/celeryd_%s stop' % env.conf['INSTANCE_NAME'])
 
 @utils.run_as_sudo
 def celery_restart():
     ''' Restart Celery '''
     with settings(warn_only=True):
-        sudo('/etc/init.d/celeryd restart')
+        sudo('/etc/init.d/celeryd_%s restart' % env.conf['INSTANCE_NAME'])
 
 @utils.run_as_sudo
 def celery_reload():
     ''' Reload Celery '''
-    sudo('/etc/init.d/celeryd reload')
+    sudo('/etc/init.d/celeryd_%s reload' % env.conf['INSTANCE_NAME'])
 
 
 @utils.run_as_sudo
 def celerybeat_setup():
     """ Setups Celerybeat. """
 
-    utils.upload_config_template('celerybeat.sh', '/etc/init.d/celerybeat', use_sudo=True)
+    utils.upload_config_template('celerybeat.sh', '/etc/init.d/celerybeat_%s' % env.conf['INSTANCE_NAME'], use_sudo=True)
 
-    if not files.exists('/etc/init.d/celeryd'):
+    if not files.exists('/etc/init.d/celeryd_%s' % env.conf['INSTANCE_NAME']):
         print "You must setup “celeryd” before “celerybeat”!!!"
         return
 
-    sudo('chmod +x /etc/init.d/celerybeat')
-    sudo('update-rc.d celerybeat defaults')
+    sudo('chmod +x /etc/init.d/celerybeat_%s' % env.conf['INSTANCE_NAME'])
+    sudo('update-rc.d celerybeat_%s defaults' % env.conf['INSTANCE_NAME'])
     celerybeat_start()
 
 
@@ -70,24 +71,24 @@ def celerybeat_setup():
 def celerybeat_start():
     ''' Start Celerybeat '''
     with settings(warn_only=True):
-        sudo('/etc/init.d/celerybeat start')
+        sudo('/etc/init.d/celerybeat_%s start' % env.conf['INSTANCE_NAME'])
 
 
 @utils.run_as_sudo
 def celerybeat_stop():
     ''' Stop Celerybeat '''
     with settings(warn_only=True):
-        sudo('/etc/init.d/celerybeat stop')
+        sudo('/etc/init.d/celerybeat_%s stop' % env.conf['INSTANCE_NAME'])
 
 
 @utils.run_as_sudo
 def celerybeat_restart():
     ''' Restart Celerybeat '''
     with settings(warn_only=True):
-        sudo('/etc/init.d/celerybeat restart')
+        sudo('/etc/init.d/celerybeat_%s restart' % env.conf['INSTANCE_NAME'])
 
 
 @utils.run_as_sudo
 def celerybeat_reload():
     ''' Reload Celerybeat '''
-    sudo('/etc/init.d/celerybeat reload')
+    sudo('/etc/init.d/celerybeat_%s reload' % env.conf['INSTANCE_NAME'])
